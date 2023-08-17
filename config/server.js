475c5c100c962365e143c341fede1d26acd6d461
hoaiht <hoaiht@basebs.com>
2023-08-17 15:30:21 +0700

    fix: build

--- config/server.js
+++ config/server.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
   proxy: true,
-  url: "/strapi",
+  // url: "/strapi",
   // url: "https://lab.connect247.vn/strapi",
   globalProxy: "/strapi",
 });
