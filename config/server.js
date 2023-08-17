3ac4f0f388ed2b2021e0929dbaf5aca6de44fa37
hoaiht <hoaiht@basebs.com>
2023-08-17 13:58:30 +0700

    fix: add proxy

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", ""),
+  // url: env("URL", ""),
   app: {
     keys: env.array("APP_KEYS"),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
-  // proxy: true,
+  proxy: true,
 });
